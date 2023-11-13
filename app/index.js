b7f64abfbe0d6b0c4eefa82d2c400cb845f60d73
leandroArraes <leandro.arraes.182@gmail.com>
2023-11-13 20:01:05 -0300

    maps

---
+++ app/index.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <MapView style={{ height: '100%', width: '100%' }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        initialRegion={{
+          latitude: -22.9108283,
+          longitude: -43.1754936,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        >
+        <Marker
+          coordinate={{ latitude: -22.9108283, longitude: -43.1754936 }}
+          pinColor="blue"
+        />
+      </MapView>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
